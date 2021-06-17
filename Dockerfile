67bd08732be4e48e1a77780f1d67a138c7f20746
Suraj Joshi <jsuraj8796@gmail.com>
2021-06-17 11:40:03 +0530

    made some changes

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ LABEL maintainer "jsuraj8796@gmail.com"
 # set a health check
 HEALTHCHECK --interval=5s \
             --timeout=5s \
-            CMD curl -f http://127.0.0.1:8000 || exit 1
+            CMD curl -f http://127.0.0.1:8001 || exit 1
 
 # tell docker what port to expose
 EXPOSE 8000
--- main.js
+++ main.js
@@ -8,5 +8,5 @@ var server = http.createServer(function (request, response) {
 });
 
 // listen on localhost:8000
-server.listen(8000);
-console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
+server.listen(8001);
+console.log("Server listening on port 8000 :  http://127.0.0.1:8001/");
